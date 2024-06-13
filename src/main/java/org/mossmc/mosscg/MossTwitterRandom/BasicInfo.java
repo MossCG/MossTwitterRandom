f724d88f5fac25b064bc49ba6d4abffc9255ebf9
MossCG <77914339+MossCG@users.noreply.github.com>
2024-06-13 18:28:31 +0800

    update version string

--- src/main/java/org/mossmc/mosscg/MossTwitterRandom/BasicInfo.java
+++ src/main/java/org/mossmc/mosscg/MossTwitterRandom/BasicInfo.java
@@ -1,7 +1,7 @@
 package org.mossmc.mosscg.MossTwitterRandom;
 
 public class BasicInfo {
-    public static String version = "V1.0.1.1.0951";
+    public static String version = "V1.1.0.1.1828";
     public static String author = "MossCG";
 
     public static String fileGetType = "";
